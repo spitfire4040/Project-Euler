8b93f9d3d463eb0a59c75528b382bf6eb1bfbc0d
Jay <spitfire4040@protonmail.com>
2019-06-19 16:28:21 -0700

    working version of 4

--- euler4.py
+++ euler4.py
@@ -1,12 +1,27 @@
-sumList = set()
+palindromes = {}
 
-for x in range(0,1000):
-	for y in range(0,1000):
+for x in range(100,1000):
+	for y in range(100,1000):
+		print(x,y)
 		product = x * y
-		if product not in sumList:
-			sumList.add(product)
-			if product == max(sumList):
-				forward = str(product)
-				reverse = forward[::-1]
-				if forward == reverse:
-					print(x,y,product)
+		forward = str(product)
+		reverse = forward[::-1]
+		if forward == reverse:
+			palindromes.update({product:[x,y]})
+
+print(palindromes)
+
+largest = 0
+first = 0
+second = 0
+
+for item in palindromes:
+	if item > largest:
+		largest = item
+		first = palindromes[item][0]
+		second = palindromes[item][1]
+
+
+print(palindromes)
+print(' ')
+print(largest,first,second)
