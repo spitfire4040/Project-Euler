0e01d8b510463cce5a448957a63c268557558fe9
Jay <spitfire4040@protonmail.com>
2019-07-01 19:28:31 -0700

    update template

--- template.py
+++ template.py
@@ -2,7 +2,11 @@ import sys
 import os
 
 def main(argv):
-	print(argv)
+	if len(argv) < 2 or len(argv) > 2:
+		print("Usage: <euler7.py><endingNumber>")
+		sys.exit()
+	else:
+		var = sys.argv[1]
 
 
 if __name__ == '__main__':
