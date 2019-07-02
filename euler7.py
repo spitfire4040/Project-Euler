50d0088f4b3764d1532833f89989a93d0d639ad3
Jay <spitfire4040@protonmail.com>
2019-07-01 19:24:47 -0700

    number 7

--- euler7.py
+++ euler7.py
@@ -1,31 +1,47 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+# What is the 10 001st prime number?
+
 import sys
 import os
 
-factors = []
+temp = []
+primes = []
 
-def find_primes(10001):
-	number = int(num)
-	inc = 1
-
-	while len(factors) < number:
-		if inc % inc == 0:
-			factors.append(x)
+def find_primes(end):
+	x = 2
+	while len(temp) < int(end):
+		flag = False
+		for y in range(2,x):
+			if x % y == 0:
+				flag = True
+		if flag == False:
+			temp.append(x)
 			print(x)
-			inc += 1
 
-	print(factors)
+		x += 1
+
+		
+
+
+			
+
+
+
+
+
 
 
 def main(argv):
-	num = argv[1]
-	find_primes(num)
+	if len(argv) < 2 or len(argv) > 2:
+		print("Usage: <euler7.py><endingNumber>")
+		sys.exit()
+	else:
+		end = sys.argv[1]
+	find_primes(end)
+	print(temp)
+
 
 
 if __name__ == '__main__':
 	main(sys.argv)
-
-
-
-# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-
-# What is the 10 001st prime number?
